9e1a9ebd3ede401f1d9776344d775a274618bf45
Guest User <Guest@Epicodus-10D.local>
2017-11-29 20:12:15 -0800

    got input data to be stored in object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 // back end logic
-function Order(name, address, size, toppings) {
+var numberToppings = [];
+
+function Order(name, address, size, toppings, price) {
 	this.name = name;
 	this.address = address;
 	this.size = size;
-	this.toppings = [];
+	this.toppings = toppings;
+	this.price = 0;
 };
 
+// var tipToppings = [];
 // front end logic
 
 $(document).ready(function() {
@@ -13,9 +17,13 @@ $(document).ready(function() {
 		event.preventDefault();
 		var inputtedName = $("#name").val();
 		var inputtedAddress = $("#address").val();
-		var inputtedSize = $("#size").val();
-		var inputtedToppings = $("#toppings").val();
-		newOrder = new Order(inputtedName, inputtedAddress, inputtedSize, inputtedToppings);
+		var inputtedSize = $("input:radio[name=size]:checked").val();
+		$("input:checkbox[name=toppings]:checked").each(function() {
+			var inputtedToppings = $(this).val();
+			numberToppings.push(inputtedToppings);
+		});
+
+		newOrder = new Order(inputtedName, inputtedAddress, inputtedSize, numberToppings);
 		console.log(newOrder);
 
 	});
